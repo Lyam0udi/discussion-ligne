75e0e773706e0cf88fb891a91f21e2e8a0f6873c
ilias77-pp <lyamoudi.ilias77@gmail.com>
2021-07-04 21:46:50 +0200

    Ajoutez des fichiers projet.

---
+++ ChatServer/Client/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Client
+{
+    partial class frmClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstClients = new System.Windows.Forms.ListBox();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtMsgs = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstClients
+            // 
+            this.lstClients.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.lstClients.ForeColor = System.Drawing.Color.Lime;
+            this.lstClients.FormattingEnabled = true;
+            this.lstClients.ItemHeight = 16;
+            this.lstClients.Location = new System.Drawing.Point(503, 47);
+            this.lstClients.Margin = new System.Windows.Forms.Padding(4);
+            this.lstClients.Name = "lstClients";
+            this.lstClients.Size = new System.Drawing.Size(111, 420);
+            this.lstClients.TabIndex = 0;
+            // 
+            // txtSend
+            // 
+            this.txtSend.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtSend.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtSend.Location = new System.Drawing.Point(4, 63);
+            this.txtSend.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(351, 128);
+            this.txtSend.TabIndex = 1;
+            this.txtSend.TextChanged += new System.EventHandler(this.txtSend_TextChanged);
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnSend.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnSend.Location = new System.Drawing.Point(363, 63);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(132, 128);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Envoi";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtMsgs
+            // 
+            this.txtMsgs.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtMsgs.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtMsgs.Location = new System.Drawing.Point(4, 223);
+            this.txtMsgs.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMsgs.Multiline = true;
+            this.txtMsgs.Name = "txtMsgs";
+            this.txtMsgs.Size = new System.Drawing.Size(491, 244);
+            this.txtMsgs.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(1, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Pseudo :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtUserName.Location = new System.Drawing.Point(93, 6);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(402, 22);
+            this.txtUserName.TabIndex = 5;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnLogin.Location = new System.Drawing.Point(503, 2);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(110, 30);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(1, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Message  :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(1, 202);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Discussion  :";
+            // 
+            // frmClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(620, 480);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMsgs);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.lstClients);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmClient";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.frmClient_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstClients;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtMsgs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ChatServer/Server/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lstUser = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Location = new System.Drawing.Point(13, 13);
+            this.btnStartStop.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(100, 390);
+            this.btnStartStop.TabIndex = 0;
+            this.btnStartStop.Text = "Lancer";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(121, 375);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(407, 28);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lstUser
+            // 
+            this.lstUser.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.lstUser.FormattingEnabled = true;
+            this.lstUser.ItemHeight = 16;
+            this.lstUser.Location = new System.Drawing.Point(121, 13);
+            this.lstUser.Margin = new System.Windows.Forms.Padding(4);
+            this.lstUser.Name = "lstUser";
+            this.lstUser.Size = new System.Drawing.Size(397, 356);
+            this.lstUser.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
+            this.ClientSize = new System.Drawing.Size(531, 416);
+            this.Controls.Add(this.lstUser);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStartStop);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Serveur";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox lstUser;
+    }
+}
+
